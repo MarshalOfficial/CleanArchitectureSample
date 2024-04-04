e041874ebeaf74b8285889efc3d31471f80fe2f4
MohadmadReza <mr.rahmatzade371@gmail.com>
2024-04-03 21:37:03 -0400

    add service registration in application and persistence

---
+++ Infrastructure/CleanArchitectureSample.Persistence/Configurations/PersistenceServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchitectureSample.Application.Contracts.Persistence;
+using CleanArchitectureSample.Persistence.Contexts;
+using CleanArchitectureSample.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace CleanArchitectureSample.Persistence.Configurations
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<CleanArchitectureDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("ConnectionString"))
+            );
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<IMemberRepository, MemberRepository>();
+            return services;
+        }
+    }
+}
